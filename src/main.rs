b707166ba21edb57b8348bdefe1f46b727926620
ainozaki <aiai10piano@gmail.com>
2024-02-05 19:13:41 +0900

    pass Args

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,13 @@ use anyhow::Result;
 use clap::Parser;
 use wasker::compiler;
 
-#[derive(Parser, Debug)]
-struct Args {
-    input_file: String,
-
-    #[arg(default_value = "./")]
-    output_dir: String,
-}
-
 fn main() -> Result<()> {
     // init logger
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    let args = Args::parse();
+    let args = compiler::Args::parse();
 
     // Compile Wasm and output ELF
-    compiler::compile_wasm_from_file(&args.input_file, &args.output_dir)?;
+    compiler::compile_wasm_from_file(&args)?;
 
     Ok(())
 }
